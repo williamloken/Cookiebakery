87ea0f7ff48987fed6f8903d4fed24f72574dad2
William <w.theonion@gmail.com>
2014-11-13 16:53:50 +0100

    PRODUCER YO

---
+++ ConsoleApplication4/Producer1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApplication4
+{
+    class CookieBakery
+    {
+        private Object thisLock = new Object();
+        Basket b = new Basket();
+        public void Stock()
+        {
+
+            lock (thisLock)
+            {
+                for (int i = 1; i < 13; i++)
+                {
+                    Thread.Sleep(677);
+                    b.Produce(i);
+
+                }
+            }
+        }
+    }
+}
